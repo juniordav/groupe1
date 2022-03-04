f36a2ced6197204ae08c93f2555accd9a4f6f324
juniordav <davtechlab@gmail.com>
2022-03-04 14:34:29 +0100

    commit

--- spacex/spacex/spacex/src/app/models/Launch.ts
+++ spacex/spacex/spacex/src/app/models/Launch.ts
@@ -3,13 +3,13 @@ import { Rocket } from "./Rocket";
 export class Launch {
     
 
-     flight_number!: number;
-     nationality!: string;
-     customer!: string;
-     land_success! : boolean;
-     land_vehicule! : boolean;
-     launch_date_local! : Date;
-     mission_name! : string;
-     rocket! : Rocket;
+   flight_number!: number;
+   launch_year!: number;
+   launch_date_utc!: Date;
+   mission_name! : string;
+   rocket! : Rocket;
+   launch_site!: any;
+   launch_success!: boolean;
+   links!: any;
      
   }
