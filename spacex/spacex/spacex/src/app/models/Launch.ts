230f80f729549f54955ddf8d835018eae3c24e06
juniordav <davtechlab@gmail.com>
2022-03-04 12:12:12 +0100

    fix getOneDetail

--- spacex/spacex/spacex/src/app/models/Launch.ts
+++ spacex/spacex/spacex/src/app/models/Launch.ts
@@ -1,16 +1,19 @@
 import { Rocket } from "./Rocket";
 
-export class Launch {
-    
+export class Launch{
 
-     flight_number!: number;
-     launch_year!: number;
-     launch_date_utc!: Date;
-     mission_name! : string;
-     rocket! : Rocket;
-     launch_site!: any;
-     launch_success!: boolean;
-     links!: any;
+ 
 
-     
-  }
+ 
+flight_number!: number;
+launch_year!: number;
+launch_date_utc!: Date;
+mission_name! : string;
+rocket! : Rocket;
+launch_site!: any;
+launch_success!: boolean;
+links!: any;
+ 
+
+
+}
